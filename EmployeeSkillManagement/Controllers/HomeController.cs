11461d9d123f6fad1dab8cb3ba5a8d9f8ca40126
shoaib <shoaib1207k@gmail.com>
2023-12-19 01:36:11 +0530

    Fixed Home Controller Error

--- EmployeeSkillManagement/Controllers/HomeController.cs
+++ EmployeeSkillManagement/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace EmployeeSkillManagement.Controllers;
 
-[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
