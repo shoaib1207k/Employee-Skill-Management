25310c07b7adef9e3a7aabcb92578bcef1a0ecca
shoaib <shoaib1207k@gmail.com>
2023-12-24 02:16:49 +0530

    Generate report feature added

---
+++ EmployeeSkillManagement/Models/ViewModels/EmployeeReportViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace EmployeeSkillManagement.Models.ViewModels
+{
+    public class EmployeeReportViewModel
+    {
+        public List<Employee>? Employees { get; set; }
+        public string PrimarySkill { get; set; } 
+    }
+}
